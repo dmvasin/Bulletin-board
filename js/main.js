847bf743a771d3b3a9d8a50b5cb2f93e804bac06
dmvasin <31286143+dmvasin@users.noreply.github.com>
2021-07-28 20:54:15 +0300

    Создана страница объявления

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 6,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+const swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+       
+        thumbs: {
+          swiper: swiper,
+        },
+      });
